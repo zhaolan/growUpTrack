0d13f122c5975aff38fb66508cf3a794a62b662c
lanzhao <lanzhao@thoughtworks.com>
2020-05-27 15:44:54 +0800

    Initial Commit

---
+++ growUpTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  growUpTrack
+//
+//  Created by ZhaoLan on 2020/5/27.
+//  Copyright © 2020 Lan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
