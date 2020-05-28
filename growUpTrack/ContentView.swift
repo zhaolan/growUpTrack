601feb6dd426375b3c780fed8e7503411a12755f
lanzhao <lanzhao@thoughtworks.com>
2020-05-28 14:32:05 +0800

    add the login page

--- growUpTrack/ContentView.swift
+++ growUpTrack/ContentView.swift
@@ -9,8 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name: String = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack {
+                TextField("宝宝昵称", text: $name)
+                Divider().frame(height: 1).padding(.horizontal, 30).background(Color.gray)
+            }
+            .padding()
+            Button(action: {
+                print("go to the home page")
+            })
+            {
+                HStack {
+                    Text("登入")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                }
+            }
+            .buttonStyle(GradientBackgroundStyle())
+
+        }
+    .padding()
+    }
+
+    
+    struct GradientBackgroundStyle: ButtonStyle {
+     
+        func makeBody(configuration: Self.Configuration) -> some View {
+            configuration.label
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding()
+                .foregroundColor(Color.white)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.white]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(40)
+                .padding(.horizontal, 20)
+        }
     }
 }
 
